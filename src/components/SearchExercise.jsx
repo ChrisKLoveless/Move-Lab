dd1ba99d97f272c9f5171b4bb5acac5e819cbf9a
ChrisKLoveless <chriskloveless@gmail.com>
2023-03-07 11:08:41 -0800

    style search page

--- src/components/SearchExercise.jsx
+++ src/components/SearchExercise.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 import PropTypes from "prop-types";
 import { fetchData } from "../utils/fetchData"
+import { Card } from "react-bootstrap";
 
 
 function SearchExercise(props) {
@@ -19,7 +21,7 @@ function SearchExercise(props) {
       }
     };
 
-    fetchData(('https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=' +  search), SearchOptions)
+    fetchData(('https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=' + search), SearchOptions)
       .then((data) => {
         console.log(data)
         setExercises(data)
@@ -32,35 +34,57 @@ function SearchExercise(props) {
   const slicedArray = exercises.slice(0, 3);
   return (
     <div>
-      <form onSubmit={submitHandler}>
-        <input
-          type="text"
-          value={search}
-          placeholder="Enter Body Part"
-          onChange={(e) => {
-            setSearch(e.target.value);
-          }} />
-        <button type="submit">Search</button>
-      </form>
-      <div>
-        <div>
+      <SearchBar>
+        <form onSubmit={submitHandler}>
+          <div className="relative">
+            <input 
+            type="text"
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}  
+            class="block w-full p-3 pl-10 mr-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search by Muscle" required />
+              <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 p-2  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+          </div>
+        </form>
+      </SearchBar>
 
-          {slicedArray.map((exercise, index) => (
-            <div key={index} className="text-center">
-              <h5>Name: {exercise.name}</h5>
-              <h5>Type: {exercise.type}</h5>
-              <h5>Body Part: {exercise.muscle}</h5>
-              <h5>Equipment: {exercise.equipment}</h5>
-              <h5>Difficulty: {exercise.difficulty}</h5>
-              <h5>Instructions: {exercise.instructions}</h5>
-              <hr />
-            </div>
-          ))}
-        </div>
-      </div>
+      <CardSection>
+        {slicedArray.map((exercise, index) => (
+          <div key={index} className="block max-w-sm p-6 text-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 className="mb-2 text-xl font-bold tracking-tight">{exercise.name}</h5>
+            <hr />
+            <h5>Type: {exercise.type}</h5>
+            <hr />
+            <h5>Body Part: {exercise.muscle}</h5>
+            <hr />
+            <h5>Equipment: {exercise.equipment}</h5>
+            <hr />
+            <h5>Difficulty: {exercise.difficulty}</h5>
+            <hr />
+            <h5>Instructions: <br />
+              {exercise.instructions}</h5>
+            <hr />
+          </div>
+        ))}
+      </CardSection >
     </div>
   )
 }
 
+const SearchBar = styled.section`
+display: flex;
+  justify-content: center;
+  margin-top: 2rem;
+  height: 15vh;
+`;
+
+const CardSection = styled.section`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  margin-top: 7rem;
+    }
+`;
 
 export default SearchExercise;
