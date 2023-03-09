ef0fee3d74c90340f53200b6790580d1852c597b
ChrisKLoveless <chriskloveless@gmail.com>
2023-03-08 16:31:09 -0800

    WIP: paginating data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Div } from "./StyledComponents";
+import { Div, CardSection } from "./StyledComponents";
+import { allExercises } from "./AllExercises";
 
 
 
@@ -12,6 +13,23 @@ function Home() {
 With Move Lab, you can customize your workout by searching for the exercise that is right for you. Our app also features animations and instructions, which will help you stay motivated and accountable on your fitness journey.
 
 Whether you're looking to build muscle, burn fat, improve your flexibility, or simply maintain your fitness level, Move Lab has what you need to reach your goals. Download the app today and start your journey towards a healthier, happier you!</p>
+
+<CardSection>
+{allExercises.map((exercise, index) => (
+          <div key={index} className="block max-w-sm p-6 text-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <span className="mb-2 text-xl font-bold tracking-tight">{exercise.name}</span>
+            <hr />
+            <span>Body Part: {exercise.bodyPart}</span>
+            <hr />
+            <span>Equipment: {exercise.equipment}</span>
+            <hr />
+            <span>Demo: <img src={exercise.gifUrl} alt="demo"/></span>
+            <hr />
+            <span>Target: {exercise.target}</span>
+          </div>
+        ))}
+
+</CardSection>
     </Div>
   )
 }
